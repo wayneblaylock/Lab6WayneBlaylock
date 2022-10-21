a1a101eec19ba1245d02e22821b27c50fd764177
Wayne Blaylock <wayne.blaylock1324@gmail.com>
2022-10-20 19:21:01 -0600

    done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿void StackNoParams(){
+    int a = 4;
+    int b = 5;
+    int c = 6;
+    int d = 7;
+    int e = 8;
+}
+
+void StackWithParams(int a, int b, int c){
+    int d = a + b + c;
+}
+
+void StackWithHeap(int a, int b, int c, string d){
+    int e = 4;
+    int f = 5;
+    string g = "Hello World";
+}
+
+void StackOver(){
+    for(int x=0; x<1000; x++){
+        StackOver();
+    }
+}
+
+StackNoParams();
+StackWithParams(1,2,3);
+StackWithHeap(1,2,3,"Hello World");
+StackOver();
